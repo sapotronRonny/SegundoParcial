a5cf91e5e59e38e5efe6d6323dd9f458979c644b
sapotronRonny <ronnychoez23@gmail.com>
2024-06-28 21:24:11 -0500

    practica_7_add

--- SEGUNDO_PARCIAL/practica_7/src/resultado/dto/update-resultado.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateResultadoDto } from './create-resultado.dto';
-
-export class UpdateResultadoDto extends PartialType(CreateResultadoDto) {}
